42ddd44b98e5e53cebccef192990e56f277c35ee
selcuksarikoz <benimpostahesabim@gmail.com>
2025-12-20 02:21:18 +0100

    feat: Migrate build system to esbuild, update scripts, and refine ignored files.

--- esbuild.js
+++ esbuild.js
@@ -16,9 +16,11 @@ const esbuildProblemMatcherPlugin = {
     build.onEnd((result) => {
       result.errors.forEach(({ text, location }) => {
         console.error(`✘ [ERROR] ${text}`);
-        console.error(
-          `    ${location.file}:${location.line}:${location.column}:`
-        );
+        if (location) {
+          console.error(
+            `    ${location.file}:${location.line}:${location.column}:`
+          );
+        }
       });
       console.log("[watch] build finished");
     });
@@ -37,6 +39,7 @@ async function main() {
     outfile: "dist/extension.js",
     external: ["vscode"],
     logLevel: "silent",
+    legalComments: production ? "none" : "inline",
     plugins: [
       /* add to the end of plugins array */
       esbuildProblemMatcherPlugin,
